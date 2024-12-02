066fb7dd6b41da8a9f639a0d51fa09d528be9ae8
Abhishek Raghav <115450330+abhiraghav23@users.noreply.github.com>
2024-12-02 13:36:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+const colorSelect = document.getElementById('colorSelect');
+const btn1 = document.getElementById('btn1');
+
+btn1.addEventListener("click", () => {
+  const selectedIndex = colorSelect.selectedIndex;
+
+  if (selectedIndex !== -1) { 
+    const selectedOption = colorSelect.options[selectedIndex].text; 
+    colorSelect.options[selectedIndex].remove();
+    alert(`${selectedOption} has been removed.`);
+  } else {
+    alert("No color selected.");
+  }
+});
